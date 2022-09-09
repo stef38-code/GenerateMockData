bf50071a451544acc1abd554a25757abcc61460f
stephane betton <stphane.betton@ag2rlamondiale.fr>
2022-09-09 13:52:26 +0200

    add email, personn, uuid

---
+++ src/main/java/org/generate/mock/email/DictionaryDomain.java
@@ -0,0 +1,9 @@
+package org.generate.mock.email;
+
+import java.util.List;
+
+public class DictionaryDomain {
+    public static List<String> domains(){
+        return List.of("@gmail.com", "@yahoo.com", "@outlook.com");
+    }
+}
--- src/main/java/org/generate/mock/email/EmailGenerator.java
+++ src/main/java/org/generate/mock/email/EmailGenerator.java
@@ -2,9 +2,9 @@ package org.generate.mock.email;
 
 import org.generate.mock.person.GeneratorUserName;
 
+import java.security.SecureRandom;
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,32 +13,55 @@ import java.util.stream.Stream;
  * @see https://medium.com/javarevisited/how-to-generate-mock-data-in-java-ff3b5f66f167
  */
 public class EmailGenerator {
-    private static final List<String> domain = List.of("@gmail.com", "@yahoo.com", "@outlook.com");
+    private final long size;
+    public EmailGenerator() {
+        this(1);
+    }
+    public EmailGenerator(long size){
+        this.size = size;
+    }
+    public List<String> build(){
+        Supplier<String> supplier = getSupplier(GeneratorUserName.generateMaleUserName()
+                .replace(" ", ".")
+                .toLowerCase(Locale.getDefault()));
+        return Stream.generate(supplier).limit(size).collect(Collectors.toList());
+    }
+
+    
+
+
+
 
     /**
      * Orginal article
      */
      public static List<String> streamMailId(long size) {
-
-
         Supplier<String> supplier = () ->{
             String userName = GeneratorUserName.generateUserName();
-            Random random = new Random();
-            int index = random.nextInt(domain.size());
-            return userName+domain.get(index);
+            return randValue(userName, DictionaryDomain.domains());
         };
 
         return Stream.generate(supplier).limit(size).collect(Collectors.toList());
     }
     public static List<String> mailMale(long size) {
-        Supplier<String> supplier = () ->{
-            String userName = GeneratorUserName.generateMaleUserName()
-                    .replace(" ",".")
-                    .toLowerCase(Locale.getDefault());
-            Random random = new Random();
-            int index = random.nextInt(domain.size());
-            return userName+domain.get(index);
-        };
+        String userName = GeneratorUserName.generateMaleUserName()
+                .replace(" ", ".")
+                .toLowerCase(Locale.getDefault());
+        Supplier<String> supplier = getSupplier(userName);
         return Stream.generate(supplier).limit(size).collect(Collectors.toList());
+    }
+
+    private static Supplier<String> getSupplier(String userName) {
+        Supplier<String> supplier = () ->{
+            List<String> domains = DictionaryDomain.domains();
+            return randValue(userName,domains);
+        };
+        return supplier;
+    }
+
+    private static String randValue(String userName,List<String> domains){
+        SecureRandom random = new SecureRandom();
+        int index = random.nextInt(domains.size());
+        return userName+domains.get(index);
     }
 }
